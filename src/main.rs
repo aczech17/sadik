56bd34d86979a67f82a40fcf2e3ed8b551a79b65
Adrian Czech <adrian-czech@outlook.com>
2023-02-14 02:12:20 +0100

    fun with deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use crate::file_info::FileInfoVector;
+use std::fs::File;
+use std::io::Read;
+use crate::file_info::{FileInfo, FileInfoVector};
 
 mod file_info;
 mod archive;
@@ -7,6 +9,22 @@ use crate::archive::archive_files;
 
 fn main()
 {
+    /*
+    let mut content: String = String::new();
+    let mut file = File::open("out").unwrap();
+    file.read_to_string(&mut content).unwrap();
+
+    //println!("{content}");
+    let s = format!("{}]", content.split("]").collect::<Vec<&str>>()[0]);
+
+    let file_info_vector: FileInfoVector = serde_json::from_str(&s).unwrap();
+
+    for file_info in file_info_vector.0
+    {
+        println!("{} {}", file_info.get_name(), file_info.get_size());
+    }
+    */
+
     const USAGE: &str = "super archiwizator i dearchiwizator kurwo";
 
     let args: Vec<String> = std::env::args().collect();
